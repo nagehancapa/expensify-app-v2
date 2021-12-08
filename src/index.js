f356144ee926901d85608c0c61a5df09313d21ab
nagehancapa <capanagehan@gmail.com>
2021-12-08 18:06:56 +0100

    added first connected component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./store";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 import { addExpense } from "./store/expenses/actions";
 import { setTextFilter } from "./store/filters/actions";
 import { getVisibleExpenses } from "./store/expenses/selectors";
@@ -23,9 +23,9 @@ console.log(store.getState());
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      {/*<Provider store={store}>*/}
-      <App />
-      {/*</Provider>*/}
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
