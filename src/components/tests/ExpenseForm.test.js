4d54f32a49669816689f1212d56b3fa9b0571c90
nagehancapa <capanagehan@gmail.com>
2021-12-25 13:24:27 +0100

    added new test cases to ExpenseForm test

--- src/components/tests/ExpenseForm.test.js
+++ src/components/tests/ExpenseForm.test.js
@@ -3,6 +3,9 @@ import { shallow } from "enzyme";
 import ExpenseForm from "../ExpenseForm";
 import expenses from "../../fixtures/expenses";
 // import toJson from "enzyme-to-json";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "jest-canvas-mock";
 
 beforeAll(() => {
   jest.useFakeTimers("modern");
@@ -25,10 +28,49 @@ test("should render ExpenseForm correctly with expense data", () => {
 
 test("should render error for invalid form submission", () => {
   const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
   wrapper.find("form").simulate("submit", {
     preventDefault: () => {},
   });
   // console.log(toJson(wrapper.find("p")));
   const error = wrapper.find("p").children();
   expect(error.isEmptyRender()).toBe(false);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test("should set description on input change", () => {
+  const value = "New description";
+  const wrapper = shallow(<ExpenseForm />);
+  //at(0) means first input
+  wrapper.find("input").at(0).simulate("change", {
+    target: { value },
+  });
+  // console.log(wrapper.find("input").get(0).props.value);
+  expect(wrapper.find("input").get(0).props.value).toBe(value);
+});
+
+test("should set description on input change test with testing library", () => {
+  render(<ExpenseForm />);
+  const input = screen.getByPlaceholderText("Description");
+  userEvent.type(input, "second description");
+  expect(screen.getByPlaceholderText("Description")).toHaveValue(
+    "second description"
+  );
+});
+
+test("should set note on textarea change", () => {
+  const value = "New note";
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find("textarea").simulate("change", {
+    target: { value },
+  });
+  // console.log(wrapper.find("textarea").get(0).props.value);
+  expect(wrapper.find("textarea").get(0).props.value).toBe(value);
+});
+
+test("should set note on textare change test with testing library", () => {
+  render(<ExpenseForm />);
+  const textarea = screen.getByPlaceholderText(/Add a note/);
+  userEvent.type(textarea, "second note");
+  expect(screen.getByPlaceholderText(/Add a note/)).toHaveValue("second note");
 });
