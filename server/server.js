360bc92d079e706c71331800831b514a0188575f
nagehancapa <capanagehan@gmail.com>
2022-01-09 11:48:05 +0100

    setup for deploy to heroku

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "..", "build");
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
@@ -9,8 +10,6 @@ app.use(express.static(publicPath));
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
-
-const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
